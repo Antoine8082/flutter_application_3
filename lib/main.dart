74d3299927ded04f37306a741fb226b5c21fd70f
Antoine C <antoine.cesbron.perso@gmail.com>
2023-05-01 19:59:35 +0200

    Cleaning

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_3/pages/wizards_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const WizardsPage(),
+      home: WizardsPage(),
     );
   }
 }
